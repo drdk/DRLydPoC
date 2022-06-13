6b805ee353c74d546f99eac1cecaf0041f7dae41
Aimee Rivers <airi@dr.dk>
2022-06-13 10:49:01 +0200

    Yay, we have radio playing!

--- features/step_definitions/app.rb
+++ features/step_definitions/app.rb
@@ -1,3 +1,39 @@
 Given("I am on the DR LYD app") do
-  $wait.until { $driver.find_element(:accessibility_id, "DR LYD").displayed? }
+  $wait.until {
+    $driver.find_element(:id, "dk.dr.radio.qa:id/btn_1").displayed?
+  }
+  4.times {
+    $driver.find_element(:id, "dk.dr.radio.qa:id/btn_1").click
+  }
+  $wait.until {
+    $driver.find_element(:id, "dk.dr.radio.qa:id/dialog_btn_positive").displayed?
+  }
+  $driver.find_element(:id, "dk.dr.radio.qa:id/dialog_btn_positive").click
+end
+
+When("I go to the Live tab") do
+  $wait.until {
+    $driver.find_element(:id, "dk.dr.radio.qa:id/navigation_item_live").displayed?
+  }
+  $driver.find_element(:id, "dk.dr.radio.qa:id/navigation_item_live").click
+  $wait.until {
+    $driver.find_element(:id, "dk.dr.radio.qa:id/header").displayed?
+  }
+end
+
+When("I touch the {string} card") do |channel|
+  $wait.until {
+    $driver.find_element(:id, "dk.dr.radio.qa:id/image").displayed?
+  }
+  $driver.find_element(:id, "dk.dr.radio.qa:id/image").click
+end
+
+Then("radio channel {string} should begin playing") do |channel|
+  $wait.until {
+    $driver.find_element(:id, "dk.dr.radio.qa:id/activity_main_player_mini").displayed?
+  }
+  $wait.until {
+    $driver.find_element(:id, "dk.dr.radio.qa:id/player_mini_play_button").displayed?
+  }
+  expect($driver.find_element(:id, "dk.dr.radio.qa:id/player_mini_play_button").contentDescription).to eq "Pause"
 end
